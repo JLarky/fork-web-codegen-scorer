836522667e60bf84bd4733302e35bf478a6d89c4
Yaroslav Lapin <ylapin@legalzoom.com>
2025-09-19 16:58:14 -0600

    gemini can't do SSR :D

--- examples/environments/lift-html/project/astro.config.mjs
+++ examples/environments/lift-html/project/astro.config.mjs
@@ -2,14 +2,9 @@
 import { defineConfig } from 'astro/config';
 import solidJs from '@astrojs/solid-js';
 import tailwindcss from '@tailwindcss/vite';
-import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: node({
-    mode: 'standalone',
-  }),
-
   vite: {
     plugins: [tailwindcss()],
   },
